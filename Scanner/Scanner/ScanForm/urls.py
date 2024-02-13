43b931cfd4251dd431090a97f5a08fd32ca7f88a
BGuragain1 <bigyanguragain01@gmail.com>
2024-02-13 20:41:26 +0545

    different changes commited

--- Scanner/Scanner/ScanForm/urls.py
+++ Scanner/Scanner/ScanForm/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.startPage,name="home"),
-    path('login', views.login,name="login"),
+    path('login', views.Login,name="login"),
     path('signup', views.signup,name="signup"),
     path('logout', views.logout,name="logout"),
     path('home',views.homePage,name="homePage"),
     path('about',views.about,name="about"),
     path('feedback',views.feedback,name="feedback"),
-    path('base',views.base, name="base"),
+    path('form',views.form,name="form"),
 ]
--- Scanner/Scanner/ScanForm/views.py
+++ Scanner/Scanner/ScanForm/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def startPage(request):
     return render(request,"index.html")
 
-def login(request):
+def Login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+          # Authenticate user
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request,user)
+            return redirect("homePage")
+        else:
+            return render(request,"login.html")
+            
     return render(request,"login.html")
 
 def signup(request):
@@ -30,8 +45,15 @@ def signup(request):
 def logout(request):
     return render(request,"index.html")
 
+# @login_required
 def homePage(request):
-    return render(request,"home.html")
+    user = request.user
+    
+    userInfo = {
+        "username" : user.username,
+        "first_name": user.first_name,
+    }
+    return render(request,"home.html",{"userInfo":userInfo})
 
 def about(request):
     return render(request,"about.html")
@@ -39,5 +61,5 @@ def about(request):
 def feedback(request):
     return render(request,"feedback.html")
 
-def base(request):
-    return render(request, "base.html")
+def form(request):
+    return render(request,"form.html")
